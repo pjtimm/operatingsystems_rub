ed4b567785cce221c0de5f04099c7c0076a852a5
Patric <patricj.timm@gmail.com>
2025-04-16 15:07:42 +0200

    added lilo

---
+++ lilo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: structs, global variables, etc.
+
+static int insertElement(int value) {
+	// TODO: implement me!
+	return -1;
+}
+
+static int removeElement(void) {
+	// TODO: implement me!
+	return -1;
+}
+
+int main (int argc, char* argv[]) {
+	printf("insert 47: %d\n", insertElement(47));
+	printf("insert 11: %d\n", insertElement(11));
+	printf("insert 23: %d\n", insertElement(23));
+	printf("insert 11: %d\n", insertElement(11));
+
+	printf("remove: %d\n", removeElement());
+	printf("remove: %d\n", removeElement());
+
+	// TODO: add more tests
+
+	exit(EXIT_SUCCESS);
+}
