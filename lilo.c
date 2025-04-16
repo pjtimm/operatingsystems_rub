6c0cb2023f8b9a41dd3f036dab38e78436bd071d
Patric <129106694+pjtimm@users.noreply.github.com>
2025-04-16 16:19:28 +0200

    implemented and tested insert function

--- lilo.c
+++ lilo.c
@@ -2,9 +2,52 @@
 #include <stdlib.h>
 
 // TODO: structs, global variables, etc.
+typedef struct ListNode
+{
+	int val;
+	struct ListNode *next; //einfache Verkettung
+}node_t;
+
+node_t *head = NULL;
 
 static int insertElement(int value) {
-	// TODO: implement me!
+	node_t *new_node = malloc(sizeof(node_t));
+	if (new_node == NULL) {
+        return -1; // malloc failed
+    }
+
+	//Werte zuweisen
+	new_node -> val = value;
+	new_node -> next = NULL;
+
+
+	if (head == NULL) //leere Liste
+	{
+		head = new_node;
+		return value;
+	}
+	else
+	{
+		node_t *current = head; 
+		
+		//Bis zum Ende der Liste laufen
+		while (current -> next != NULL)
+		{
+			if (current -> val == value) //Wert existiert bereits in der Liste
+			{
+				return -1;
+			}
+			current = current -> next;
+		}
+
+		if (current -> val == value) //Value vom letzten Knoten überprüfen
+			{
+				return -1;
+			}
+
+		current -> next = new_node;
+		return value;
+	}
 	return -1;
 }
 
